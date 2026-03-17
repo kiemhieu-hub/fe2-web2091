e1b9f7346298350417f2e8cf8976bb78ec749961
kiemhieu-hub <nguyenhieu4mat@gmail.com>
2026-03-17 15:57:22 +0700

    lab4

---
+++ src/pages/lab4.tsx
@@ -0,0 +1,46 @@
+import { useMutation } from "@tanstack/react-query";
+import { Button, Checkbox, Form, Input } from "antd";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+export default function lab42() {
+    const mutation = useMutation({
+        mutationFn: async (data: any) => {
+            await axios.post("http://localhost:3000/stoties", data);
+        },
+        onSuccess: () => {
+            toast.success("Thêm truyện thành công");
+        },
+
+        onError: () => {
+            toast.error("Có lỗi xảy ra");
+        },
+    });
+
+    const onAdd = (values: any) => {
+        mutation.mutate(values);
+    };
+    return (
+        <Form layout="vertical" onFinish={onAdd} style={{ maxWidth: 500 }}>
+            <Form.Item
+                label="Tên truyện"
+                name="title"
+                rules={[{ required: true, message: "Nhập tên truyện" }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item label="Mô tả" name="description">
+                <Input.TextArea rows={4} />
+            </Form.Item>
+
+            <Form.Item name="active" valuePropName="checked">
+                <Checkbox>Active</Checkbox>
+            </Form.Item>
+
+            <Button type="primary" htmlType="submit" loading={mutation.isPending}>
+                Thêm truyện
+            </Button>
+        </Form>
+    );
+};
